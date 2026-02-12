2fd1f19cdb9dec6e7d3a4a3e98c4a14b67bd4695
urbanIntrovert <trefoil72@gmail.com>
2026-02-12 19:41:18 +0000

    fix: ALLOWED_HOSTS for local + Heroku

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kg2nw*!f2$$9%)s20^c9ucrynl2+s(ojz7!we!thw5-oh$^*6p'
+SECRET_KEY = config("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = [
-    'portfolio-2026-leanne-7489bcf03221.herokuapp.com',
+    "127.0.0.1",
+    "localhost",
+    "portfolio-2026-leanne-7489bcf03221.herokuapp.com",
 ]
 
 
