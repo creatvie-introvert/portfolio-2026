7b90892f21e2f5794a27f543c860597967f0f56a
urbanIntrovert <trefoil72@gmail.com>
2026-02-19 15:44:35 +0000

    feat(portfolio): add work template and routes

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
 
+
+def home(request):
+    return render(request, "portfolio/home.html")
+
+
+def work(request):
+    return render(request, "portfolio/work.html")
